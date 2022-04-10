eb516771c115e8de28d438c1f4e527c78251458a
Ori Marom <ori.marom10@gmail.com>
2022-04-10 11:42:57 +0300

    Updated instructions page styling

--- src/pages/QuizInstructions.js
+++ src/pages/QuizInstructions.js
@@ -1,17 +1,16 @@
-import React, {  Fragment } from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import LifebuoyIcon from "mdi-react/LifebuoyIcon";
 import LightningIcon from "mdi-react/LightningBoltIcon";
-import imageNum1 from "../assets/img/imageNum1.jpg";
-import imageNum2 from "../assets/img/imageNum2.jpg";
-import imageNum3 from "../assets/img/imageNum3.jpg";
-import imageNum4 from "../assets/img/imageNum4.jpg";
+import imageNum1 from "../assets/img/playScreen.PNG";
+import imageNum2 from "../assets/img/selectQuestions.PNG";
+import imageNum3 from "../assets/img/correctAnswerCut.png";
 
 const QuizInstructions = () => (
   <Fragment>
     <Helmet>
-     <title>Instructions Page</title>
+      <title>Instructions Page</title>
     </Helmet>
     <div className="instructions container">
       <div className="instruction-title">How to play the game</div>
@@ -22,27 +21,33 @@ const QuizInstructions = () => (
           Each correct answer is 1 point
           <img src={imageNum1} alt="Quiz instruction #1" />
         </li>
-        <li>This is the image2 title</li>
+        <li>You can also select the question Category, Difficulty, Type and Amount by clicking the Select Questions button </li>
         <img src={imageNum2} alt="Quiz instruction #2" />
         <li>
-          Each game has 3 lifelines
-          <ul id="sublist">
+          Correct answer will be celebrated
+        </li>
+        <img src={imageNum3} alt="Quiz instruction #3" />
+        <li>
+          Each game has 3 lifelines:
+          
             <li>1 50-50 chances</li>
             <li>2 hints</li>
-          </ul>
+      
         </li>
         <li>
-          Selecting a 50-50 lifeline
-          <LifebuoyIcon className="lifeline-icon" size={24} /> will leave only 2 options
-          <img src={imageNum3} alt="Quiz instruction #3" />
+          Selecting a 50:50 lifeline
+          <LifebuoyIcon className="lifeline-icon" size={24} /> will leave only 2
+          options
         </li>
         <li>
-          Selecting a hint
-          <LightningIcon className="lightning-icon" size={24} /> will remove one incorrect option
-        <img src={imageNum4} alt="Quiz instruction #4" />
+        Selecting a Hint
+          <LightningIcon className="lightning-icon" size={24} /> will remove one
+          incorrect option
         </li>
+  
+        
         <li>You cn quit whenever you want</li>
-        <li>You can add yourself to the Leaderboard if you like</li>
+        <li>You can add yourself to the Leaderboard at the end of the game</li>
         <div className="back-button-container">
           <Link to="/" className="back-button">
             Back
